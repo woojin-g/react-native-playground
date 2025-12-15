9ff4e35baf2aa9e67db3dae08df8d5bf8bd2f1c9
김우진 <woojin@gim-ujin-ui-MacBookPro.local>
2025-12-16 01:26:04 +0900

    study: AppRegistry.registerComponent

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+// [study] AppRegistry.registerComponent 함수
+// iOS의 AppDelegate와 Android의 MainActivity가 이 등록된 컴포넌트를 찾아 렌더링한다.
 AppRegistry.registerComponent(appName, () => App);
